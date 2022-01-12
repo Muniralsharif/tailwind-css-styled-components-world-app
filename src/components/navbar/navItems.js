dfbaac2818c6df34256ce7c398274f466cb49dd8
Muniralsharif <44524993+Muniralsharif@users.noreply.github.com>
2022-01-12 07:53:08 -0800

    Adding Navbar Component

--- src/components/navbar/navItems.js
+++ src/components/navbar/navItems.js
@@ -2,16 +2,27 @@ import React from "react";
 import Link from "next/link";
 import styled from "styled-components";
 import tw from "twin.macro";
-import {Slide as Menu} from 'react-burger-menu'
+import { slide as Menu } from "react-burger-menu";
+import { useMediaQuery } from "react-responsive";
+import { screens } from "../responsive";
+import menuStyle from "./menuStyle";
 const ListContainer = styled.ul`
     ${tw`
         flex
         list-none
     `};
 `;
-const NavItem = styled.li`
-    ${tw`
-    text-xs
+const NavItem = styled.li(({ menu }) => [
+    menu
+        ? tw`
+    text-white
+    text-xl
+    mb-3
+    font-bold
+    focus:text-white
+    `
+        : tw`
+    text-sm
     md:text-base
     text-black
     font-medium
@@ -22,15 +33,35 @@ const NavItem = styled.li`
     duration-300
     ease-in-out
     hover:text-gray-700
-
-    `};
-`;
+    `,
+]);
 const NavLink = styled(Link)`
     ${tw`
 
     `};
 `;
 const NavItems = () => {
+    const isMobile = useMediaQuery({ maxWidth: screens.sm });
+    if (isMobile) {
+        return (
+            <Menu right styles={menuStyle}>
+                <ListContainer>
+                    <NavItem menu>
+                        <NavLink href="/">Home</NavLink>
+                    </NavItem>
+                    <NavItem menu>
+                        <NavLink href="/cars">Cars</NavLink>
+                    </NavItem>
+                    <NavItem menu>
+                        <NavLink href="/services">Services</NavLink>
+                    </NavItem>
+                    <NavItem menu>
+                        <NavLink href="/contact">Contact Us</NavLink>
+                    </NavItem>
+                </ListContainer>
+            </Menu>
+        );
+    }
     return (
         <ListContainer>
             <NavItem>
